0658eff52f5d3ccd5cf442c25a58609c210387cd
Jnu <9artregt@gmail.com>
2023-06-22 22:50:08 +0900

    Initial Commit

---
+++ SecondTask/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SecondTask
+//
+//  Created by Jinwoo on 2023/06/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SecondTask/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SecondTask
+//
+//  Created by Jinwoo on 2023/06/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
